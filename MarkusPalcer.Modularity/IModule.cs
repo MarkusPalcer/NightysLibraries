e718f0c192ea4886d5b9bea4e13ab693f9dab40a
Markus Palcer <MasterOfVDL@GMail.com>
2023-09-19 20:19:15 +0200

    initial commit

---
+++ MarkusPalcer.Modularity/IModule.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarkusPalcer.Modularity;
+
+public interface IModule
+{
+    void RegisterTypes(IServiceCollection serviceCollection, ImmutableArray<Assembly> assemblies);
+}
---
+++ MarkusPalcer.Modularity/ServiceProviderBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarkusPalcer.Modularity;
+
+public class ServiceProviderBuilder
+{
+    private List<Assembly> _assemblies = new();
+    
+    public ServiceProviderBuilder WithAssembly(Assembly assembly)
+    {
+        _assemblies.Add(assembly);
+        return this;
+    }
+
+    public ServiceProviderBuilder WithAssemblyOf<T>()
+    {
+        return WithAssembly(typeof(T).Assembly);
+    }
+
+    public IServiceProvider Build()
+    {
+        var assemblies = _assemblies.ToImmutableArray();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(cls => cls.AssignableTo<IModule>()).As<IModule>()
+            .WithSingletonLifetime());
+        var modules = serviceCollection.BuildServiceProvider().GetRequiredService<IEnumerable<IModule>>().ToArray();
+
+
+        serviceCollection = new ServiceCollection();
+        foreach (var module in modules)
+        {
+            module.RegisterTypes(serviceCollection, assemblies);
+        }
+
+        return serviceCollection.BuildServiceProvider();
+    }
+}
