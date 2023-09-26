a4faf7c4a86a55deb978bb6f6faea14e0e089010
Markus Palcer <markus.palcer@zuehlke.com>
2023-09-26 11:13:13 +0200

    Extensions used implicitely

--- MarkusPalcer.AutoWire.Module/Extensions.cs
+++ MarkusPalcer.AutoWire.Module/Extensions.cs
@@ -1,9 +1,12 @@
-﻿using MarkusPalcer.Modularity;
+﻿using JetBrains.Annotations;
+using MarkusPalcer.Modularity;
 
 namespace MarkusPalcer.AutoWire.Module;
 
+[UsedImplicitly]
 public static class Extensions
 {
+    [UsedImplicitly]
     public static ServiceProviderBuilder WithAutoWire(this ServiceProviderBuilder self)
     {
         return self.WithAssemblyOf<Module>();
