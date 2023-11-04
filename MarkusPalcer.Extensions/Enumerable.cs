6dcb76616ac6c65577b04ff09df6f7342187eb55
Markus Palcer <MasterOfVDL@GMail.com>
2023-11-04 22:19:08 +0100

    add IEnumerable<T>.ToObservableCollection() extension method

---
+++ MarkusPalcer.Extensions/Enumerable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MarkusPalcer.Extensions;
+
+public static class Enumerable
+{
+    /// <summary>
+    /// Converts the <see cref="IEnumerable{T}"/> to an observable collection
+    /// </summary>
+    public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> self) =>
+        new ObservableCollection<T>(self);
+}
