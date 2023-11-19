bbe4fe844925f6c3d403b506f744f3eb578ee9eb
Markus Palcer <MasterOfVDL@GMail.com>
2023-11-19 14:02:03 +0100

    add extension method to add file system abstraction to container builder

---
+++ MarkusPalcer.FileSystem.Module/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using MarkusPalcer.Modularity;
+
+namespace MarkusPalcer.FileSystem.Module;
+
+[UsedImplicitly]
+public static class Extensions
+{
+    [UsedImplicitly]
+    public static ServiceProviderBuilder WithFileSystemAbstractions(this ServiceProviderBuilder self)
+    {
+        return self.WithAssemblyOf<Module>();
+    }
+}
