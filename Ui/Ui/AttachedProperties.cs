ff79b58b858069a7910f025c517bb6b0cf59de67
Markus Palcer <MasterOfVDL@GMail.com>
2024-03-07 08:32:57 +0100

    wip

---
+++ Ui/Ui/AttachedProperties.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Ui;
+
+public class AttachedProperties
+{
+    public static readonly DependencyProperty InvertedForegroundProperty = DependencyProperty.RegisterAttached(
+        "InvertedForeground",
+        typeof(Brush),
+        typeof(AttachedProperties),
+        new FrameworkPropertyMetadata(Brushes.White, FrameworkPropertyMetadataOptions.Inherits));
+
+    public static void SetInvertedForeground(DependencyObject element, Brush value)
+    {
+        element.SetValue(InvertedForegroundProperty, value);
+    }
+
+    public static Brush GetInvertedForeground(DependencyObject element)
+    {
+        return (Brush)element.GetValue(InvertedForegroundProperty);
+    }
+}
