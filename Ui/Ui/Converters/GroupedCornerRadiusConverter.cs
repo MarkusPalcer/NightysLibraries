733990cde28d3cda135899c55af69ed8592fd163
Markus Palcer <MarkusPalcer@users.noreply.github.com>
2024-03-08 19:47:44 +0100

    add template for ListBox (#4)

--- Ui/Ui/Converters/GroupedCornerRadiusConverter.cs
+++ Ui/Ui/Converters/GroupedCornerRadiusConverter.cs
@@ -8,16 +8,21 @@ public class GroupedCornerRadiusConverter : IMultiValueConverter
 {
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        var baseValue = values[0] as CornerRadius? ?? throw new InvalidOperationException("First argument to GroupedCornerRadiusConverter must be the base corner radius to modify");
-        if (values[1] == DependencyProperty.UnsetValue) values[1] = Grouping.Single;
-        var grouping = values[1] as Grouping? ?? throw new InvalidOperationException("Second argument to GroupedCornerRadiusConverter must be the grouping to apply");
+        var baseValue = values[0] as CornerRadius? ??
+                        throw new InvalidOperationException(
+                            "First argument to GroupedCornerRadiusConverter must be the base corner radius to modify");
+        if (values[1] == DependencyProperty.UnsetValue) values[1] = GroupPosition.Single;
+        var grouping = values[1] as GroupPosition? ??
+                       throw new InvalidOperationException(
+                           "Second argument to GroupedCornerRadiusConverter must be the grouping to apply");
+
 
         return grouping switch
         {
-            Grouping.Single => baseValue,
-            Grouping.First => new CornerRadius(baseValue.TopLeft, 0, 0, baseValue.BottomLeft),
-            Grouping.Middle => new CornerRadius(0),
-            Grouping.Last => new CornerRadius(0, baseValue.TopRight, baseValue.BottomRight, 0),
+            GroupPosition.Single => baseValue,
+            GroupPosition.First => baseValue with { TopRight = 0, BottomRight = 0 },
+            GroupPosition.Middle => new CornerRadius(0),
+            GroupPosition.Last => baseValue with { TopLeft = 0, BottomLeft = 0 },
 #pragma warning disable CA2208
             _ => throw new ArgumentOutOfRangeException(nameof(grouping))
 #pragma warning restore CA2208
--- Ui/Ui/Grouping.cs
+++ Ui/Ui/Grouping.cs
@@ -1,9 +1,22 @@
-﻿namespace Ui;
+﻿using System.Windows;
 
-public enum Grouping
+namespace Ui;
+
+public class Grouping
 {
-    Single,
-    First,
-    Middle,
-    Last
+    public static readonly DependencyProperty PositionProperty = DependencyProperty.RegisterAttached(
+        "Position",
+        typeof(GroupPosition),
+        typeof(Grouping),
+        new PropertyMetadata(GroupPosition.Single));
+
+    public static void SetPosition(DependencyObject element, GroupPosition value)
+    {
+        element.SetValue(PositionProperty, value);
+    }
+
+    public static GroupPosition GetPosition(DependencyObject element)
+    {
+        return (GroupPosition) element.GetValue(PositionProperty);
+    }
 }
